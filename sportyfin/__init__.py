93521f6cd70eb57790cedf42ce8ec4afb0725197
NoamStanis <noamstanislawski@gmail.com>
2025-03-27 15:51:07 -0700

    Update build system

--- sportyfin/__init__.py
+++ sportyfin/__init__.py
@@ -5,8 +5,7 @@ pyexample.
 An example python library.
 """
 
-__version__ = "0.1.0"
-__author__ = 'Stephen Hudson'
-__credits__ = 'Argonne National Laboratory'
+__version__ = "1.1.0"
+__author__ = 'Noam'
 
 from .sportyfin import *
